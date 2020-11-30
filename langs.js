872b6685a9a4149dd5e218fa63c05bb2ff404d0f
kar <hi@kar.gs>
2020-11-30 12:10:52 +0100

    add langs file to share in various scripts

---
+++ langs.js
@@ -0,0 +1,50 @@
+export default {
+    "langs": ["pl", "de", "es", "it", "hi", "ru", "bg", "tr", "ja", "id", "cs", "zh-Hant", "ar", "fi", "ro", "pt-BR", "fr", "hu", "nl"],
+    "langs-android": {
+        "id": "in",
+        "zh-Hant": "zh",
+        "pt-BR": "b+pt+BR"
+    },
+    "langs-android-res": {
+        "pl": "pl-rPL",
+        "de": "de-rDE",
+        "es": "es-rES",
+        "it": "it-rIT",
+        "hi": "hi-rIN",
+        "ru": "ru-rRU",
+        "bg": "bg-rBG",
+        "tr": "tr-rTR",
+        "ja": "ja-rJP",
+        "id": "in-rID",
+        "cs": "cs-rCZ",
+        "zh-Hant": "zh-rTW",
+        "ar": "ar-rSA",
+        "fi": "fi-rFI",
+        "ro": "ro-rRO",
+        "pt-BR": "pt-rBR",
+        "fr": "fr-rFR",
+        "hu": "hu-rHU",
+        "nl": "nl-rNL"
+    },
+    "langs-web4": {
+        "pl": "pl_PL",
+        "de": "de_DE",
+        "es": "es_ES",
+        "it": "it_IT",
+        "hi": "hi_IN",
+        "ru": "ru_RU",
+        "bg": "bg_BG",
+        "tr": "tr_TR",
+        "ja": "ja_JP",
+        "id": "id_ID",
+        "cs": "cs_CZ",
+        "zh-Hant": "zh_TW",
+        "ar": "ar_SA",
+        "fi": "fi_FI",
+        "ro": "ro_RO",
+        "pt-BR": "pt_BR",
+        "fr": "fr_FR",
+        "hu": "hu_HU",
+        "nl": "nl_NL"
+    }
+}
